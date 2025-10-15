cbd5c24d7b921a46cdf607ea134112a7c2e45e1d
Viswajith2003 <viswajithviswa715@gmail.com>
2025-10-16 01:03:17 +0530

    edit delete set

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -1,24 +1,47 @@
 import React, { useEffect, useRef, useState } from "react";
+import { IoMdDoneAll } from "react-icons/io";
+import { FiEdit } from "react-icons/fi";
+import { MdDelete } from "react-icons/md";
 
 function Todo() {
   const [text, setText] = useState("");
   const [todos, setTodos] = useState([]);
+  const [editId, setEditId] = useState(0);
 
-  const inputRef=useRef();
+  const inputRef = useRef();
 
   const addItems = () => {
-    setTodos([...todos, text]);
-    console.log(todos);
-    setText("");
+    if (text !== "") {
+      setTodos([...todos, { list: text, id: Date.now(), status: false }]);
+      console.log(todos);
+      setText("");
+    }
+    if(editId){
+      const editTodo=todos.find((todo) => todo.id === editId)
+      const updateTodo=todos.map((to)=> to.id === editTodo.id ? {...to, list: text} : to)
+      setTodos(updateTodo)
+      setEditId(0)
+      setText('')
+    }
   };
 
   const hangleForm = (e) => {
     e.preventDefault();
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     inputRef.current.focus();
-  })
+  });
+
+  const onDelete = (id) => {
+    setTodos(todos.filter((item) => item.id !== id));
+  };
+
+  const onEdit = (id) => {
+    let edit = todos.find((item) => item.id === id);
+    setText(edit.list);
+    setEditId(edit.id);
+  };
 
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
@@ -42,7 +65,7 @@ function Todo() {
             onClick={addItems}
             className="bg-teal-900 text-white px-6 py-3 rounded-lg font-semibold hover:bg-teal-950 transition-colors border border-teal-600 "
           >
-            ADD
+            {editId ? "EDIT" : "ADD"}
           </button>
         </form>
 
@@ -51,9 +74,28 @@ function Todo() {
             {todos.map((item) => (
               <li
                 key={item.id}
-                className="text-lg mt-3 px-5 rounded-lg py-2 text-amber-300 bg-black"
+                className="text-lg mt-3 px-5 rounded-lg py-2 text-amber-300 bg-black flex justify-between"
               >
-                {item}
+                {item.list}
+                <span className="flex gap-3 pt-1">
+                  <IoMdDoneAll
+                    className="text-green-400"
+                    id="complete"
+                    title="Complete"
+                  />
+                  <FiEdit
+                    onClick={() => onEdit(item.id)}
+                    className="text-blue-400"
+                    id="edit"
+                    title="Edit"
+                  />
+                  <MdDelete
+                    className="text-red-500"
+                    id="delete"
+                    title="Delete"
+                    onClick={() => onDelete(item.id)}
+                  />
+                </span>
               </li>
             ))}
           </ul>
